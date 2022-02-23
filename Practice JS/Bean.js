9be1f41b6d714706701934f2af7f211b57b43b13
Frimlis <54984270+Frimlis@users.noreply.github.com>
2022-02-23 21:26:42 +0300

    we improving (probably)

--- Practice JS/Bean.js
+++ Practice JS/Bean.js
@@ -1,12 +1,12 @@
-function beanCounting(bean) {
+function beanCounting(bean,whatCount) {
     let numberBean = 0;
     let count = 0;
 while (bean.length>=numberBean) {
- if (bean[numberBean]==='B') {
+ if (bean[numberBean]===whatCount) {
      count=count+1;
  }
     numberBean=numberBean+1;    
 }
 return count;
 }
-console.log(beanCounting('BarBarbarBbBB'))
+console.log(beanCounting('BarBarbarBbBB','B'))

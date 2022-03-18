e1ef0e11be18b346353387f81e3a557ebb44ef41
Frimlis <54984270+Frimlis@users.noreply.github.com>
2022-03-18 23:06:00 +0300

    whatever

---
+++ Calculator/calculator.js
@@ -0,0 +1,4 @@
+let number1=document.getElementById(number1);
+let number2=document.getElementById(number2);
+let operation=document.getElementById(operation);
+let result = document.getElementById(result);

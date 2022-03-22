12ad61d7202d21608cb72c137c6e517ba8fb3966
Frimlis <54984270+Frimlis@users.noreply.github.com>
2022-03-22 22:25:02 +0300

    its not working and idk why, give up for today

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -2,3 +2,10 @@ let number1=document.getElementById(number1);
 let number2=document.getElementById(number2);
 let operation=document.getElementById(operation);
 let result = document.getElementById(result);
+
+document.getElementById('button_+').onclick = function() {displayButton()};
+
+function displayButton() {
+ // document.getElementById('operation').value = '+';
+ alert("hi");
+}

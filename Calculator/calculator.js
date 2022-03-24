30ad9a6508bd610bb5589d2cee2c037b29adfffe
Frimlis <54984270+Frimlis@users.noreply.github.com>
2022-03-24 23:03:15 +0300

    I did enough yesterday...

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,12 +1,20 @@
-//let number1=document.getElementById(number1);
-//let number2=document.getElementById(number2);
-//let operation=document.getElementById(operation);
-//let result = document.getElementById(result);
+//let number1=document.getElementById('number1');
+//let number2=document.getElementById('number2');
+//let operation=document.getElementById('operation');
+//let result = document.getElementById('result');
 
 function displayButton() {
   document.getElementById('operation').value = this.innerHTML;
+}
+function AC_reset() {
+  //number1.reset();
+  //number2.reset();
+  document.getElementById('operation').value = "";
+ // result.reset();
 }
 document.getElementById('button_+').addEventListener("click", displayButton);
 document.getElementById('button_-').addEventListener("click", displayButton);
 document.getElementById('button_*').addEventListener("click", displayButton);
 document.getElementById('button_/').addEventListener("click", displayButton);
+
+document.getElementById('reset').addEventListener("click", AC_reset);

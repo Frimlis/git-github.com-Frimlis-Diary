115243d23963a1ef461bbe2c4bfd756000a21a0d
Frimlis <54984270+Frimlis@users.noreply.github.com>
2022-04-04 21:52:15 +0300

    fixing

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,37 +6,45 @@
 function addNumberButton() {
   if (document.getElementById('radio_number1').checked) {
     let number1_in_string = document.getElementById('number1').value;
-    number1_in_string = '' + number1_in_string + this.innerHTML;
+    number1_in_string = '' + number1_in_string + this.textContent;
     document.getElementById('number1').value = number1_in_string;
   } else {
     let number2_in_string = document.getElementById('number2').value;
-    number2_in_string = number2_in_string + this.innerHTML;
+    number2_in_string = number2_in_string + this.textContent;
     document.getElementById('number2').value = number2_in_string;
   }
 };
-
+function strip (number) {
+  return (parseFloat(number.toPrecision(12)));
+}
 function equation() {
   let number1_in_string = parseFloat(document.getElementById('number1').value);
   let number2_in_string = parseFloat(document.getElementById('number2').value);
   if (document.getElementById('operation').value == "+") {
-    document.getElementById('result').value = number1_in_string + number2_in_string;
+    let equa = number1_in_string + number2_in_string;
+    document.getElementById('result').value = strip(equa);
   } else if (document.getElementById('operation').value == "-") {
-    document.getElementById('result').value = number1_in_string - number2_in_string;
+    let equa = number1_in_string - number2_in_string;
+    document.getElementById('result').value = strip(equa);
   } else if (document.getElementById('operation').value == "*") {
-    document.getElementById('result').value = number1_in_string * number2_in_string;
+    let equa = number1_in_string * number2_in_string;
+    document.getElementById('result').value = strip(equa);
   } else if (document.getElementById('operation').value == "/") {
-    document.getElementById('result').value = number1_in_string / number2_in_string;
+    let equa = number1_in_string * number2_in_string;
+    document.getElementById('result').value = strip(equa);
   }
   document.getElementById('radio_number1').checked = true;
   document.getElementById('radio_number2').checked = false;
 }
 
 function displayButton() {
-  document.getElementById('operation').value = this.innerHTML;
+  document.getElementById('operation').value = this.textContent;
   document.getElementById('radio_number1').checked = false;
   document.getElementById('radio_number2').checked = true;
 };
 function AC_reset() {
+  let number1_in_string = parseFloat(document.getElementById('number1').value);
+  let number2_in_string = parseFloat(document.getElementById('number2').value);
   document.getElementById('number1').value = "";
   number2=document.getElementById('number2').value = "";
   document.getElementById('operation').value = "";
